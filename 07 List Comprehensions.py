cc1d5061f947a614a2e492f2a93390c241403d9e
David <44362463+Gencid@users.noreply.github.com>
2018-11-11 14:22:13 +0100

    Replaced descriptions for links

--- 07 List Comprehensions.py
+++ 07 List Comprehensions.py
@@ -1,5 +1,4 @@
-"""Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line
-of Python that takes this list a and makes a new list that has only the even elements of this list in it."""
+"""https://www.practicepython.org/exercise/2014/03/19/07-list-comprehensions.html"""
 
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 b = [item for item in a if item % 2 == 0]
